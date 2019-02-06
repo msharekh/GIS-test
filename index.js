5dd3c76db73c14e46db949a92ea0bc41a62aac4f
Mishal Alsharekh <mishal.sharekh@gmail.com>
2019-02-06 13:56:16 +0300

    fixed not complete

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
+require(["esri/Map", "esri/views/MapView", "esri/Graphic"], function(
+  Map,
+  MapView,
+  Graphic
+) {
   // Code to create the map and view will go here
   var map = new Map({
     basemap: "streets"
@@ -9,6 +13,41 @@ require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
     zoom: 15, // Sets zoom level based on level of detail (LOD)
     center: [46.70240879, 24.68045354] // Sets center point of view using longitude,latitude
   });
+
+  // Create a line geometry with the coordinates of the line
+  var polyline = {
+    type: "polyline", // autocasts as new Polyline()
+    paths: [
+      [46.69361115, 24.67822194],
+      [46.61938906, 24.78706598],
+      [46.73929453, 24.70382094]
+    ]
+  };
+
+  // Create a simple line symbol for rendering the line in the view
+  var lineSymbol = {
+    type: "simple-line", // autocasts as new SimpleLineSymbol()
+    color: [226, 119, 40], // RGB color values as an array
+    width: 4
+  };
+
+  // Create a simple object containing useful information relating to the feature
+  var lineAtt = {
+    Name: "Keystone Pipeline", // The name of the pipeline
+    Owner: "TransCanada", // The owner of the pipeline
+    Length: "3,456 km" // The length of the pipeline
+  };
+
+  // Create the graphic
+  var polylineGraphic = new Graphic({
+    geometry: polyline, // Add the geometry created in step 4
+    symbol: lineSymbol, // Add the symbol created in step 5
+    attributes: lineAtt // Add the attributes created in step 6
+  });
+
+  // Add the graphic to the view's default graphics view
+  // If adding multiple graphics, use addMany and pass in the array of graphics.
+  view.graphics.add(polylineGraphic);
 });
 
 function confirmFileSubmit() {
