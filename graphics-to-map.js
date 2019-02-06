bb94a24e8d6350ed710c8addd34d9f8f549ce274
Mishal Alsharekh <mishal.sharekh@gmail.com>
2019-02-06 12:54:44 +0300

    many mrker types

--- graphics-to-map.js
+++ graphics-to-map.js
@@ -24,14 +24,18 @@ require([
   dom,
   on
 ) {
+  /* CREATE MAP */
   map = new Map("mapDiv", {
     basemap: "streets",
-    center: [46.70240879, 24.68045354],
-    zoom: 11
+    center: [-25.312, 34.307],
+    zoom: 3
   });
+  /* MAP ON LOAD CALL INITTOOLBAR*/
   map.on("load", initToolbar);
 
   // markerSymbol is used for point and multipoint, see http://raphaeljs.com/icons/#talkq for more examples
+
+  /* MARKER SYMBOL */
   var markerSymbol = new SimpleMarkerSymbol();
   markerSymbol.setPath(
     "M16,4.938c-7.732,0-14,4.701-14,10.5c0,1.981,0.741,3.833,2.016,5.414L2,25.272l5.613-1.44c2.339,1.316,5.237,2.106,8.387,2.106c7.732,0,14-4.701,14-10.5S23.732,4.938,16,4.938zM16.868,21.375h-1.969v-1.889h1.969V21.375zM16.772,18.094h-1.777l-0.176-8.083h2.113L16.772,18.094z"
@@ -82,6 +86,7 @@ require([
     var symbol;
     if (evt.geometry.type === "point" || evt.geometry.type === "multipoint") {
       console.log("evt.geometry:", evt.geometry);
+
       symbol = markerSymbol;
     } else if (
       evt.geometry.type === "line" ||
@@ -92,31 +97,7 @@ require([
       symbol = fillSymbol;
     }
 
+    /* ADD SYMBOL TO MAP */
     map.graphics.add(new Graphic(evt.geometry, symbol));
   }
 });
-
-function confirmFileSubmit() {
-  var input = document.getElementById("fileUpload"); // get the input
-  var file = input.files[0]; // assuming single file, no multiple
-  var reader = new FileReader();
-  let lng = "";
-  let lat = "";
-  reader.onload = function(e) {
-    var text = reader.result; // the entire file
-
-    var lines = text.split("\n"); // split lines to array
-    for (let l = 1; l < lines.length; l++) {
-      const line = lines[l];
-      lng = line.split(",")[1];
-      lat = line.split(",")[2];
-      console.log(lng, ":", lat);
-    }
-
-    // lng = text.split("\n")[1].split(",")[1];
-    // lat = text.split("\n")[1].split(",")[2];
-  };
-
-  reader.readAsText(file, "UTF-8"); // or whatever encoding you're using
-  // UTF-8 is default, so this argument
-}
